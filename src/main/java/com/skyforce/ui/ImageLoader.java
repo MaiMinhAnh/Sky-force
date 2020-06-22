fa862fe70547ab6ac91d0c63f0f11fcf793e5490
MaiMinhAnh <anhmai121099@gmail.com>
2020-06-22 15:28:25 +0700

    first commit

---
+++ src/main/java/com/skyforce/ui/ImageLoader.java
@@ -0,0 +1,30 @@
+package com.skyforce.ui;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ImageLoader {
+    public static BufferedImage image;
+    public static BufferedImage entities;
+    public static BufferedImage enemy, player, myPlane;
+
+    public static void init() {
+//        src\main\resources\airplane.png
+       image = imageLoader("/sea.jpg");
+       entities = imageLoader("/airplane.png");
+       enemy = entities.getSubimage(0, 0,85, 90);
+       player = entities.getSubimage(85, 0, 95, 90);
+       myPlane = imageLoader("/airplane2.png").getSubimage(85, 0, 95, 90);
+    }
+
+    public static BufferedImage imageLoader(String path) {
+        try {
+            return ImageIO.read(ImageLoader.class.getResource(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+}
